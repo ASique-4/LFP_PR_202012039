e06de6394bd558c284b0e95a132b4c2334b15b0e
ASique-4 <3555772080101@ingenieria.usac.edu.gt>
2022-02-07 09:50:12 -0600

    Agregando gui

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import easygui as gui
 def pedirNumeroEntero():
  
     correcto=False
@@ -19,19 +20,23 @@ while not salir:
     print ("1. Cargar Datos")
     print ("2. Cargar Instrucciones")
     print ("3. Analizar")
-    print ("4. Salir")
+    print ("4. Hacer Reportes")
+    print ("5. Salir")
      
     print ("Elige una opcion")
  
     opcion = pedirNumeroEntero()
  
     if opcion == 1:
-        print ("Opcion 1")
+        Datos = gui.filesavebox()
+        print(Datos)
     elif opcion == 2:
         print ("Opcion 2")
     elif opcion == 3:
         print("Opcion 3")
     elif opcion == 4:
+        print("Opcion 4")    
+    elif opcion == 5:
         salir = True
     else:
         print ("Introduce un numero entre 1 y 3")
