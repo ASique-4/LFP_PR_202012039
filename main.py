7164cf414d80b6906a1ec60792b6db8510582fa1
ASique-4 <3555772080101@ingenieria.usac.edu.gt>
2022-02-07 13:25:54 -0600

    Escoger Archivos

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import easygui as gui
+
+from msilib.schema import File
+
+print('>< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><')
 def pedirNumeroEntero():
  
     correcto=False
@@ -14,7 +17,7 @@ def pedirNumeroEntero():
  
 salir = False
 opcion = 0
- 
+            
 while not salir:
  
     print ("1. Cargar Datos")
@@ -26,19 +29,63 @@ while not salir:
     print ("Elige una opcion")
  
     opcion = pedirNumeroEntero()
- 
+
     if opcion == 1:
-        Datos = gui.filesavebox()
-        print(Datos)
+        print('>< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><')
+        import PySimpleGUI as sg
+        sg.theme('Dark Grey 13')   
+
+        layout = [[sg.Text('Escribe la ruta o navega entre los archivos')],
+          [sg.Input(), sg.FileBrowse()],
+          [sg.OK(), sg.Cancel()]]
+
+        window = sg.Window('Datos', layout)
+
+
+        event, Datos = window.read()
+        
+        if Datos[0] == '' or Datos[0] == None:
+            print('No escogiste ningún archivo')
+        else:
+            print('Escogiste el archivo: ',Datos[0])
+        window.close()
+
+        print('>< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><')
     elif opcion == 2:
-        print ("Opcion 2")
+        print('>< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><')
+
+        import PySimpleGUI as sg
+        sg.theme('Dark Grey 13') 
+
+        layout = [[sg.Text('Escribe la ruta o navega entre los archivos')],
+          [sg.Input(), sg.FileBrowse()],
+          [sg.OK(), sg.Cancel()]]
+
+
+        window = sg.Window('Datos', layout)
+
+
+        event, Instrucciones = window.read()
+        
+        if Instrucciones[0] == '' or Instrucciones[0] == None:
+            print('No escogiste ningún archivo')
+        else:
+            print('Escogiste el archivo: ',Instrucciones[0])
+        window.close()
+        print('>< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><')
     elif opcion == 3:
+        print('>< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><')
         print("Opcion 3")
+        print('>< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><')
     elif opcion == 4:
-        print("Opcion 4")    
+        print('>< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><')
+        print("Opcion 4")   
+        print('>< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><') 
     elif opcion == 5:
+
         salir = True
     else:
-        print ("Introduce un numero entre 1 y 3")
+        print ("Introduce un numero entre 1 y 5")
  
 print ("Fin")
+
